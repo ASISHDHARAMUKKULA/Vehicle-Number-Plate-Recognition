1a4f6c3f5e15225a8158cafd6078bfbb1de18515
ASISH DHARAMUKKULA <asishdharamukkula@gmail.com>
2019-05-11 20:18:44 +0530

    Add files via upload

---
+++ six.py
@@ -0,0 +1,9 @@
+import numpy as n
+import matplotlib.pyplot as pt
+import pandas as pd
+import cycler as c
+from sklearn.tree import DecisionTreeClassifier
+
+data=pd.read_csv('train.csv')
+
+print(data)
